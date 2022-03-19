ec6bf0a87f0b36cd03ee9b4a2eadeed706668c98
sonnylr <98625057+sonnylr@users.noreply.github.com>
2022-03-19 09:40:17 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html> 
+<body> 
+    
+<?php 
+// foreach loop 
+
+$array = array ("Jou","Kezia","Gibson","Stevania","Beri");
+foreach ($array as $loopdata) { 
+
+    echo "My name is " .$loopdata. "<br>"; 
+    
+} 
+
+?> 
+</body> 
+</html>
